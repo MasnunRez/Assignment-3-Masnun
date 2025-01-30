3e871958966b979e79f59c3c6b02e6775b69cba6
MasnunRez <masnun.rezon93@gmail.com>
2025-01-31 00:32:36 +0600

    Header section completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,7 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {
-        colors:{
-            
-            "yellow-clr":"",
-            "btn-color": "",
-        }
-      },
+      extend: {},
     },
     plugins: [],
   }
